5ae878e346c44dfdc6840aebd366d1e010755d79
CHUN888 <552269348@qq.com>
2019-12-23 20:45:36 +0800

    work18

--- work18/index.js
+++ work18/index.js
@@ -1,26 +1,22 @@
-function send() {
-    let sno = document.getELementById('sno').value
-    let name = document.getELementById('name').value 
+function send(){
+    let sno = document.getElementById('sno').value
+    let name = document.getElementById('name').value
     let content = document.getElementById('content').value
     let xmlhttp;
-    if (window. XMLHttpRequest) { 
-
-        xmLhttp = new XMLHttpRequest();
-    } else {
-
-    xmLhttp = new ActiveX0bject("Microsoft.XMLHTTP"); 
+    if(window.XMLHttpRequest){
+        xmlhttp = new XMLHttpRequest();
+    }else{
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
-
-
-    xmlhttp.open('post','http://localhost:8080/ajax', true)
+    //xmlhttp.open('post','http://localhost:8080/ajax',true)
+    xmlhttp.open('post','http://139.9.81.203:8090/ajax',true)
     xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-    xmlhttp.send('sno=' + sno +'&name=' + name+"&content="+content)
-
-    xmLhttp. onreadystatechange = function () {
-        if (xmLhttp. readyState == 4 && xmLhttp.status == 200) { 
-           let ajaxData=JSON.parse(xmlhttp.responseText)
-           result = ajaxData.reverse().map((val)=>{return JSON.stringify(val)})
-           document.getElementById("result").innerHTML = result.join('<br>');
+    xmlhttp.send('sno='+sno+'&name='+name+"&content="+content)
+    xmlhttp.onreadystatechange = function(){
+        if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
+            let ajaxData=JSON.parse(xmlhttp.responseText)
+            result = ajaxData.reverse().map((val)=>{return JSON.stringify(val)})
+            document.getElementById("result").innerHTML = result.join('<br>');
         }
-    }    
+    }
 }
